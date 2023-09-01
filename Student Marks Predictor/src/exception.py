eeb6b8aa0b9dbdd64de1ccd385ce11690285adb8
NikhilPaulzagde <118707097+NikhilPaulzagde@users.noreply.github.com>
2023-09-01 12:09:16 +0530

    final commit

---
+++ Student Marks Predictor/src/exception.py
@@ -0,0 +1,26 @@
+import sys
+from src.logger import logging
+
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+     file_name,exc_tb.tb_lineno,str(error))
+
+    return error_message
+'''This function will tell thst how message/error should looklike'''
+
+    
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    
+    '''own customexception which is populated by error_message_details
+    '''
+
+    def __str__(self):
+        return self.error_message
+    '''When we raise custom exception it will print the message'''
+
