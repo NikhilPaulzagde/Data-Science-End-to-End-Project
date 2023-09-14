9d82f883bdd3ccfe8b2dfa43688bffcd82fd9ec6
NIKHIL PAULZAGDE <118707097+NikhilPaulzagde@users.noreply.github.com>
2023-09-14 16:45:41 +0530

    Setup and requirements are completed

---
+++ Concrete Strenght Prediction/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(filepath:str) -> List[str]:
+
+    requirements = []
+
+    with open(filepath) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [i.replace("\n","")   for i in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+setup(name='Concrete Strenght',
+      version='0.0.0.1',
+      description='PipeLine Project FOr ML',
+      author='Nikhil Paulzgade',
+      author_email='nickpaulzagde@gmail.com',
+      packages=find_packages(),
+      install_requires=get_requirements('requirements.txt')
+     )
